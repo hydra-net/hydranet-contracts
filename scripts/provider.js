e93d0434251ac2992903f9050299a2a38d63432b
rimatik <slaven@rimatik.hr>
2022-04-26 12:20:28 +0200

    providers exporter

--- scripts/provider.js
+++ scripts/provider.js
@@ -1,10 +1,22 @@
 const { ethers } = require("hardhat");
 require("dotenv").config();
-const { ARBITRUM_INFURA_URL } = process.env;
+const { ARBITRUM_INFURA_URL, ARBITRUM_INFURA_RINKEBY_URL, INFURA_RINKEBY_URL } = process.env;
 
 const provider = new ethers.providers.JsonRpcProvider(ARBITRUM_INFURA_URL, {
     chainId: Number(42161),
     name: "Arbitrum",
 });
 
+const providerArbitrumRinkeby = new ethers.providers.JsonRpcProvider(ARBITRUM_INFURA_RINKEBY_URL, {
+    chainId: Number(421611),
+    name: "arbitrum-rinkeby",
+});
+
+const providerRinkeby = new ethers.providers.JsonRpcProvider(INFURA_RINKEBY_URL, {
+    chainId: Number(4),
+    name: "rinkeby",
+});
+
 exports.arbitrum = provider;
+exports.arbitrumRinkeby = providerArbitrumRinkeby;
+exports.providerRinkeby = providerRinkeby;
